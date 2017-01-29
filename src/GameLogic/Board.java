12cfc98ea5561088b458ee853739fb8f980c503d
Reece Wood <rmw530@cca-ug04-15065.cs.bham.ac.uk>
2017-01-29 14:06:55 +0000

    trying to merge board

--- src/GameLogic/Board.java
+++ src/GameLogic/Board.java
@@ -1,14 +1,41 @@
 package GameLogic;
 
+import java.util.ArrayList;
+
 public class Board {
 	
-	private String string = "";
+	private ArrayList<PhysObject> objects;
+	private ArrayList<PhysObject> changed;
+	private boolean freeState;
+	
+	public Board(){
+		this.objects = new ArrayList<PhysObject>();
+		this.changed = new ArrayList<PhysObject>();
+		
+		this.freeState = false;
+	}
+	
+	private void freeSim() {
+		//TODO
+	}
+	
+	public void updateFrame(Move move) {
+		if(freeState) { // If the engine is in free-physics mode then the move is irrelevant,
+			freeSim();  // just simulate another frame.
+		}
+		else {
+			//TODO
+			//If left/right then move a certain amount
+			//If jump and on floor then set y-velocity to a certain amount.
+		}
+	}
 	
 	/**
 	 * Used on the client-side, receiving an update string from the server.
 	 * @param update The update string.
 	 */
 	public void update(String update) {
+		
 	}
 	
 	/**
@@ -22,7 +49,11 @@ public class Board {
 	 * Used on the server-side to send an update to all the client for how their boards should look.
 	 * @return The update sent.
 	 */
-	public String getUpdate() {
-		return null;
+	public ArrayList<PhysObject> getUpdate() {
+		return objects;
+	}
+	
+	public ArrayList<PhysObject> getChanged() {
+		return changed;
 	}
 }
