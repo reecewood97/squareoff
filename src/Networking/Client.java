2d7f601ec023beed8b56708171c7aa3cab486bc2
djs568 <djs568@student.bham.ac.uk>
2017-02-03 12:20:44 +0000

    Didn't really do anything

--- src/Networking/Client.java
+++ src/Networking/Client.java
@@ -11,27 +11,39 @@ public class Client {
 	private ClientSender sender;
 	private ClientReceiver receiver;
 	
-	public Client(String host, int port) {
+	public Client() {
 		socket = null;
 		toServer = null;
 		fromServer = null;
 	}
 	
-	public void connect(String ip, int port) throws UnknownHostException, IOException {
-		socket = new Socket(ip, port);
-		toServer = new PrintStream(socket.getOutputStream());
-		fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		
+	public void connect(String ip, int port) throws UnknownHostException {
+		try {
+			socket = new Socket(ip, port);
+			toServer = new PrintStream(socket.getOutputStream());
+			fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+			
 		sender = new ClientSender(toServer);
 		receiver = new ClientReceiver(fromServer);
-		
+			
 		sender.start();
-	    receiver.start();
+		receiver.start();
 	}
 	
-	public void disconnect() throws IOException {
-		socket.close();
-		sender.close();
-		receiver.close();
+	public void disconnect() {
+		try {
+			socket.close();
+			sender.close();
+			receiver.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
--- src/Networking/ServerTest.java
+++ src/Networking/ServerTest.java
@@ -1,9 +1,66 @@
 package Networking;
 
+import java.net.UnknownHostException;
+
 public class ServerTest {
 
 	public static void main(String[] args) {
-
+		Server server = new Server(4444);
+		server.start();
+		
+		Client c1 = new Client();
+		try {
+			c1.connect("127.0.0.1", 4444);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}	
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
