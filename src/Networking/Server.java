6fd00713ad75511fd9633e6003306c86b97a5bb4
ksk523 <ksk523@student.bham.ac.uk>
2017-02-17 14:12:36 +0000

    fixed david's code

--- src/Networking/Server.java
+++ src/Networking/Server.java
@@ -59,7 +59,7 @@ public class Server extends Thread {
 				while(true){
 			
 					if(!aigenned && table.getSender().inGame()) {
-						AI ai = new AI(0, 0, board);
+						AI ai = new AI(0, 0, 0, board);
 					    aigenned = true;
 					}
 					board.input("None");
