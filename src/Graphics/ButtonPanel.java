f142e4f2b7fa4fdce6e0c408d23db9f2a1c8ce13
fxl506 <fxl506@student.bham.ac.uk>
2017-02-27 22:38:38 +0000

    button panel updates

--- src/Graphics/ButtonPanel.java
+++ src/Graphics/ButtonPanel.java
@@ -1,11 +1,12 @@
 package Graphics;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import GameLogic.Board;
 import UserInterface.mainMenu;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import Audio.*;
 
@@ -16,19 +17,21 @@ public class ButtonPanel extends JPanel{
 	private boolean music_on = true;
 	private boolean first = true;
 	private Audio audio;
-	private Screen screen;
-	private NewWeaponsMenu newmenu; // = new WeaponsMenu();
-	private boolean firstopen;
-	
+	private Board board;
 	
 	public ButtonPanel(Screen screen, Board board) {
 		
 		super();
-		this.screen = screen;
-		//opened = false;
-		setLayout(new BorderLayout());
+		
+		JPanel mainpanel = new JPanel();
+		mainpanel.setLayout(new BorderLayout());
+		add(mainpanel);
+		mainpanel.setBackground(Color.WHITE);
+		//mainpanel.setBounds(0, 0, screenwidth, height);
+		
+		this.board = board;
 	
-		//back to main menu
+		//exit button
         ImageIcon image2 = new ImageIcon("Files/Images/cross.png");
         JButton exit = new JButton(image2);
 		exit.addActionListener(e -> openMainMenu(screen,board));
@@ -37,10 +40,11 @@ public class ButtonPanel extends JPanel{
         exit.setFocusPainted(false); 
         exit.setOpaque(false);
 		
+        //start audio
         audio = new Audio();
 		audio.startBackgroundMusic();
 		
-		//toggle sound
+		//toggle sound button
         ImageIcon image3 = new ImageIcon("Files/Images/speaker1.png");
         JButton sound = new JButton(image3);
         sound.addActionListener(e -> ToggleBackgroundMusic(sound));
@@ -48,33 +52,41 @@ public class ButtonPanel extends JPanel{
 		sound.setContentAreaFilled(false); 
 		sound.setFocusPainted(false); 
 		sound.setOpaque(false);
-	
-		add(sound,BorderLayout.WEST);
-		add(exit, BorderLayout.EAST);
 		
-		firstopen=true;
-		//newmenu = new NewWeaponsMenu(board);
-		
-	
+		//weapons menu button
+		JButton weapons = new JButton("weapons menu");
+	    weapons.addActionListener(e -> showWeaponsMenu());
+		weapons.setBorderPainted(false); 
+		weapons.setContentAreaFilled(false); 
+		weapons.setFocusPainted(false); 
+		weapons.setOpaque(false);
+			
+		//add buttons to panel
+		mainpanel.add(sound,BorderLayout.WEST);
+		mainpanel.add(exit, BorderLayout.EAST);
+		mainpanel.add(weapons, BorderLayout.CENTER);
+
 
 	}
 	
 	public void openMainMenu(Screen screen, Board board){
 	
 		board.notifyQuit();
+		
+		screen.setVisible(false);
+		//mainMenu mainmenu = new mainMenu();
+		mainMenu.showUI();
+		
+		
+	}
+	
+	public void showWeaponsMenu(){
+		
 		Audio audioforclick = new Audio();
 		audioforclick.click();
 		
-		NewWeaponsMenu menu = new NewWeaponsMenu();
+		NewWeaponsMenu menu = new NewWeaponsMenu(board);
 		menu.open();
-		//screen.setVisible(false);
-		//mainMenu mainmenu = new mainMenu();
-		//mainmenu.launchMenu();
-		
-		
-		
-		
-		
 		
 	}
 	public void ToggleBackgroundMusic(JButton button){
@@ -86,13 +98,13 @@ public class ButtonPanel extends JPanel{
 		
 			if(music_on){
 				
-				System.out.println("music on");
+				//System.out.println("music on");
 				button.setOpaque(true);
 				audio.endBackgroundMusic();
 			}
 			else{
 				
-				System.out.println("music off");
+				//System.out.println("music off");
 				button.setOpaque(false);
 				audio = new Audio();
 				audio.startBackgroundMusic();
