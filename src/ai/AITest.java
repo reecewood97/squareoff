bf9ab36f8fda8a781e9182c18b804173e17860bd
yxl457 <yxl457@cs.bham.ac.uk>
2017-03-03 13:56:31 +0000

    Fixed calculation. Added calculation of movements for normal AI. Currently working on calculation of attack path for normal AI.

--- src/ai/AITest.java
+++ src/ai/AITest.java
@@ -11,7 +11,7 @@ public class AITest {
 	public static void main(String[] args) {
 		Board board = new Board();
 		
-		AI ai = new EasyAI(2, 0, 0, board);
+		AI ai = new DifficultAI(2, 0, 0, board);
 		
 		ai.changeAIPos();
 		
@@ -19,11 +19,19 @@ public class AITest {
 		System.out.println(ai.getAIPos() + " \n");
 //		System.out.println(ai.getFinalDestination());
 		
+//		ai.determineState();
 		System.out.println(ai.getAIPos());
+		
+		ai.determineResult();
 		
 		double velocity_chosen = ai.getVelocity();
 		double angle_chosen = ai.getAngle();
+		System.out.println(velocity_chosen + " " + angle_chosen);
 		
+		ai.alterResult();
+		
+		velocity_chosen = ai.getVelocity();
+		angle_chosen = ai.getAngle();
 		System.out.println(velocity_chosen + " " + angle_chosen);
 	}
 
