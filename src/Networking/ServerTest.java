4272f3129571c9774926cf71cdab04eaa4718e6e
djs568 <djs568@student.bham.ac.uk>
2017-03-22 15:29:01 +0000

    Commit

--- src/Networking/ServerTest.java
+++ src/Networking/ServerTest.java
@@ -3,16 +3,23 @@ package Networking;
 public class ServerTest {
 
 	public static void main(String[] args) {
-		Server server = new Server(4444);
-		server.start();
+//		Server server = new Server(4444);
+//		server.start();
+//		
+//		Client bob = new Client("Bob");
+//		bob.connect("127.0.0.1", 4444);
+//		
+//		Client jerry = new Client("Jerry");
+//		jerry.connect("127.0.0.1", 4444);
+//
+//		server.startGame();
 		
-		Client bob = new Client("Bob");
-		bob.connect("127.0.0.1", 4444);
+		String s = "hi";
+		String t = "hi";
 		
-		Client jerry = new Client("Jerry");
-		jerry.connect("127.0.0.1", 4444);
-
-		server.startGame();
+		
+		waitForNotEqual(s, t);
+		System.out.println("hello");
 	}	
 	
 	public static void wait(int millis) {
@@ -24,14 +31,16 @@ public class ServerTest {
 		}
 	}
 	
-//	public static void waitForNotEqual(Object ) {
-//		try {
-//			Thread.sleep(millis);
-//		}
-//		catch(InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void waitForNotEqual(Object ob1, Object ob2) {
+		while(ob1.equals(ob2)) {
+			try {			
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
 
 
