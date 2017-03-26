13e92e7be8ce26c17428fe3d8b1d9e1dec7c0a90
fxl506 <fxl506@cs.bham.ac.uk>
2017-03-26 23:59:29 +0100

    audio in suite

--- src/Test/JunitTestSuite.java
+++ src/Test/JunitTestSuite.java
@@ -3,6 +3,7 @@ package Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import Audio.AudioJUnit;
 import GameLogic.GameLogicJUnit;
 import GameLogic.PhysicsTest;
 import Graphics.GraphicsJUnit;
@@ -14,12 +15,13 @@ import ai.AIJUnit;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
    GameLogicJUnit.class,
-   //NetworkingJUnit.class,
-   //PhysicsTest.class,
+   NetworkingJUnit.class,
+   PhysicsTest.class,
    AIJUnit.class,
    UserInterfaceJUnit.class,
    GraphicsJUnit.class,
-   HangerOnJUnit.class
+   HangerOnJUnit.class,
+   AudioJUnit.class
 })
 public class JunitTestSuite {   
 }
